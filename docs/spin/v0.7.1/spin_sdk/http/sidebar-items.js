2703188615c9c5421eb37ed0636f119c01aadc4a
Fermybot <fermybot@fermyon.com>
2023-01-04 17:13:49 +0000

    Update docs/spin/v0.7.1

---
+++ docs/spin/v0.7.1/spin_sdk/http/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["internal_server_error","Helper function to return a 500 Internal Server Error response."],["not_found","Helper function to return a 404 Not Found response."]],"type":[["Request","The Spin HTTP request."],["Response","The Spin HTTP response."]]};
