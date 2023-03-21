d29f0431c29fb9d6f7f4ea9b9101b2185cb1659a
Fermybot <fermybot@fermyon.com>
2023-03-21 17:55:49 +0000

    Update docs/spin/v1.0.0

---
+++ docs/spin/v1.0.0/spin_sdk/mysql/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DbDataType","DbValue","Error","MysqlError","ParameterValue"],"fn":["execute","query"],"struct":["Column","RowSet"],"trait":["Decode"],"type":["Row"]};
