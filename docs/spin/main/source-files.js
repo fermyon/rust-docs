3a2460382e2ba8ac4c93ffeade809aa3c8d9c80c
Fermybot <fermybot@fermyon.com>
2023-06-15 18:09:04 +0000

    Update docs/spin/main

--- docs/spin/main/source-files.js
+++ docs/spin/main/source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "spin_app":["",[],["host_component.rs","lib.rs","locked.rs","metadata.rs","values.rs"]],\
-"spin_core":["",[],["host_component.rs","io.rs","lib.rs","limits.rs","store.rs"]],\
+"spin_core":["",[],["host_component.rs","io.rs","lib.rs","limits.rs","preview1.rs","store.rs"]],\
 "spin_sdk":["",[["http",[],["router.rs"]]],["key_value.rs","lib.rs","mysql.rs","outbound_http.rs","pg.rs"]],\
 "version":["",[],["version.rs"]]\
 }');
