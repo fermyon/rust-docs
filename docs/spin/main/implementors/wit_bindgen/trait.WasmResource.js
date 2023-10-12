62d894be5c7015aaace8653dd7d55086ac7fe182
Fermybot <fermybot@fermyon.com>
2023-10-12 15:09:15 +0000

    Update docs/spin/main

--- docs/spin/main/implementors/wit_bindgen/trait.WasmResource.js
+++ docs/spin/main/implementors/wit_bindgen/trait.WasmResource.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"spin_sdk":[["impl WasmResource for <a class=\"struct\" href=\"spin_sdk/sqlite/struct.Connection.html\" title=\"struct spin_sdk::sqlite::Connection\">Connection</a>"],["impl WasmResource for <a class=\"struct\" href=\"spin_sdk/key_value/struct.Store.html\" title=\"struct spin_sdk::key_value::Store\">Store</a>"]]
+"spin_sdk":[["impl WasmResource for <a class=\"struct\" href=\"spin_sdk/key_value/struct.Store.html\" title=\"struct spin_sdk::key_value::Store\">Store</a>"],["impl WasmResource for <a class=\"struct\" href=\"spin_sdk/sqlite/struct.Connection.html\" title=\"struct spin_sdk::sqlite::Connection\">Connection</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
