6ba61ee3532cf04a893eac72162d35d58fcbde61
Fermybot <fermybot@fermyon.com>
2023-10-24 16:04:58 +0000

    Update docs/spin/main

--- docs/spin/main/spin_sdk/sidebar-items.js
+++ docs/spin/main/spin_sdk/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["http_component","redis_component"],"macro":["http_router"],"mod":["config","http","key_value","llm","mysql","pg","redis","sqlite"]};
+window.SIDEBAR_ITEMS = {"attr":["http_component","redis_component"],"macro":["http_router"],"mod":["http","key_value","llm","mysql","pg","redis","sqlite","variables"]};
