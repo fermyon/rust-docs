c7d754a8f2840e5b92cc88dd26c66e337a8ce2c1
Fermybot <fermybot@fermyon.com>
2023-11-01 03:25:59 +0000

    Update docs/spin/v2.0.0-rc.1

---
+++ docs/spin/v2.0.0-rc.1/trait.impl/wasmtime/store/context/trait.AsContext.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"spin_core":[["impl&lt;T&gt; <a class=\"trait\" href=\"https://docs.rs/wasmtime/14.0.3/wasmtime/store/context/trait.AsContext.html\" title=\"trait wasmtime::store::context::AsContext\">AsContext</a> for <a class=\"struct\" href=\"spin_core/struct.Store.html\" title=\"struct spin_core::Store\">Store</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
