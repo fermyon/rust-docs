363eddbe1c4a39b8baf8a07a68572d2be0c34edc
Fermybot <fermybot@fermyon.com>
2023-11-10 18:37:00 +0000

    Update docs/spin/v2.0.1

---
+++ docs/spin/v2.0.1/spin_sdk/sqlite/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error","Value"],"struct":["Connection","QueryResult","Row","RowResult"]};
---
+++ docs/spin/v2.0.1/trait.impl/wasmtime/store/context/trait.AsContextMut.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"spin_core":[["impl&lt;T&gt; AsContextMut for <a class=\"struct\" href=\"spin_core/struct.Store.html\" title=\"struct spin_core::Store\">Store</a>&lt;T&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
