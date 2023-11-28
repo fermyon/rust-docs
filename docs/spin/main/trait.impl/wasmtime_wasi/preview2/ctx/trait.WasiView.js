05da92ef669e1f31e8a344f89abc8ded5cf00188
Fermybot <fermybot@fermyon.com>
2023-11-28 18:40:39 +0000

    Update docs/spin/main

--- docs/spin/main/trait.impl/wasmtime_wasi/preview2/ctx/trait.WasiView.js
+++ docs/spin/main/trait.impl/wasmtime_wasi/preview2/ctx/trait.WasiView.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"spin_core":[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>&gt; WasiView for <a class=\"struct\" href=\"spin_core/struct.Data.html\" title=\"struct spin_core::Data\">Data</a>&lt;T&gt;"]]
+"spin_core":[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a>&gt; <a class=\"trait\" href=\"https://docs.rs/wasmtime-wasi/15.0.0/wasmtime_wasi/preview2/ctx/trait.WasiView.html\" title=\"trait wasmtime_wasi::preview2::ctx::WasiView\">WasiView</a> for <a class=\"struct\" href=\"spin_core/struct.Data.html\" title=\"struct spin_core::Data\">Data</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
